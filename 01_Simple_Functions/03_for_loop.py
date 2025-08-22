8bcac280fd41ecb76e0910b120e1034b818152be
Ahmad Salehi <d.salehi.1992@gmail.com>
2025-08-22 11:56:07 +0200

    Add files via upload

---
+++ 01_Simple_Functions/03_for_loop.py
@@ -0,0 +1,12 @@
+for i in range(1,10):
+    print(i, i**i)
+friends = ['Yuri', 'Ali','Chris']
+for this_one in friends:
+    print('Hi', this_one)
+
+
+
+"""
+This code iterates over a list of names using a 'for' loop,
+and prints a personalized greeting for each element in the list.
+"""
---
+++ 01_Simple_Functions/Numbers.py
@@ -0,0 +1,2 @@
+f = open('/home/ahmad/Programming/Python_learning/01_simple_functions/06_Numbers_File')
+f.read()
